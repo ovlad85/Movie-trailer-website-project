31584a00bec16bfc47c52fd964b63e84009271b5
ovlad85 <vladimir.olenic@gmail.com>
2016-05-10 08:34:57 +0300

    removed unnecessary code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,14 +1,10 @@
-# Lesson 3.4: Make Classes
-# Mini-Project: Movies Website
 
-# In this file, you will define instances of the class Movie defined
-# in media.py. After you follow along with Kunal, make some instances
-# of your own!
+# Mini-Project: Movies Website
 
 # After you run this code, open the file fresh_tomatoes.html to
 # see your webpage!
 
-# https://www.udacity.com/course/viewer#!/c-nd000/l-4185678656/e-991358856/m-1013629064
+
 
 import media
 import fresh_tomatoes
